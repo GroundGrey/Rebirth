4cff6cbfe99e9bf410d4aed4c0c02ff7e3496b6e
GroundGrey <47766364+GroundGrey@users.noreply.github.com>
2020-12-21 12:14:28 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-AOS.init();
-// 待辦事項註解
-//1 (美術)主視覺圖欄位等圖
-//英文引號較佳
-//3 (美術)埃及相關美術圖--已有連結
-//4桌游還要放嗎(開會討論)
+$(document).ready(function () {
+  $(".title").attr({
+    "data-aos": "zoom-in",
+    "data-aos-duration": "2000"
+  });
+  setTimeout(() => {
+    AOS.init();
+  }, 120);
+});
+const sounds = ["Rebirth"];
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
 
-//6 (遊戲程式)遊戲載點添加--MAC不一定有
-//7 (文字內容)埃及知識的內容與參考資料
-//8 (文字內容)桌游介紹
-//9 (未定案)遊戲遊玩影片
-//10(網頁程式)字體更改、滑入效果統一、字體大小放大、背景音樂撥放
-//先註解掉的區域：影片連結、遊戲連結、部分知識
+  btn.addEventListener("click", () => {
+    stopSounds();
+
+    document.getElementById(sound).play();
+  });
+
+  document.querySelector("#buttons").appendChild(btn);
+});
+
+function stopSounds() {
+  sounds.forEach((sound) => {
+    const audio = document.getElementById(sound);
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
