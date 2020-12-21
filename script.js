9608728580dc5896996d55af78a97a96c1b4ed87
GroundGrey <47766364+GroundGrey@users.noreply.github.com>
2020-12-21 11:07:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,10 @@
 AOS.init();
-
-
-
-//音樂相關
-
-const sounds = ['theme', 'gasp', 'end']
-
-sounds.forEach(sound => {
-    const btn = document.createElement('button');
-    btn.classList.add('btn');
-    btn.innerText = sound;
-
-    btn.addEventListener('click', () => {
-        stopSounds();
-
-        document.getElementById(sound).play();
-    })
-
-    document.querySelector('#buttons').appendChild(btn);
-})
-
-function stopSounds() {
-    sounds.forEach(sound => {
-        const audio = document.getElementById(sound);
-        audio.pause();
-        audio.currentTime = 0;
-    })
-}
-
-//音樂相關
-
-
-
-
-
-
-
-
 // 待辦事項註解
 //1 (美術)主視覺圖欄位等圖
 //英文引號較佳
 //3 (美術)埃及相關美術圖--已有連結
 //4桌游還要放嗎(開會討論)
-//加賽特ㄉ介紹欄啦
+
 //6 (遊戲程式)遊戲載點添加--MAC不一定有
 //7 (文字內容)埃及知識的內容與參考資料
 //8 (文字內容)桌游介紹
